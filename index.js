efbc8c7ad37c2a0514605fc092798c97593d3cf6
Kai Davenport <kaiyadavenport@gmail.com>
2016-12-19 14:05:02 +0000

    passing test

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var objectPath = require('object-path')
+
+function getMissing(data, obj, basepath){
+  return Object.keys(obj || {}).reduce(function(missing, key){
+    var value = obj[key]
+    var path = basepath.concat([key])
+    if(typeof(value) == 'boolean'){
+      return objectPath.has(data, path) ?
+        missing :
+        missing.concat([path])
+    }
+    else{
+      return missing.concat(getMissing(data, value, path))
+    }
+  }, [])
+}
+
 module.exports = function(data, schema){
-  
+  return getMissing(data, schema, []).map(function(path){
+    return path.join('.')
+  })
 }
